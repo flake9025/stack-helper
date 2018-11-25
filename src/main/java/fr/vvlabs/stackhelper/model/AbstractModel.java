eaba89c21f9c5eb02a072559ae2108e95415d4c4
Vincent Villain <flake9025@gmail.com>
2018-11-25 19:24:11 +0100

    fix create with generated key

--- src/main/java/fr/vvlabs/stackhelper/model/AbstractModel.java
+++ src/main/java/fr/vvlabs/stackhelper/model/AbstractModel.java
@@ -2,13 +2,18 @@ package fr.vvlabs.stackhelper.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 
-import org.springframework.data.jpa.domain.AbstractPersistable;
+import org.springframework.data.domain.Persistable;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
@@ -17,22 +22,33 @@ import lombok.ToString;
  * @param <K> the primary key type
  */
 @MappedSuperclass
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper=false)
 @ToString
-public abstract class AbstractModel<K extends Serializable> extends AbstractPersistable<K> {
-	
+public abstract class AbstractModel<K extends Serializable> implements Persistable<K> {
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected K id;
+   
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
 	// ===========================================================
 	
-	/* (non-Javadoc)
-	 * @see org.springframework.data.jpa.domain.AbstractPersistable#setId(java.io.Serializable)
-	 * Override this method to get a public setId, otherwise create and update will not work !
-	 */
 	@Override
-	public void setId(final K id) {
-		super.setId(id);
-    }
+	public K getId() {
+		return id;
+	}
+
+	@Override
+	@Transient // DATAJPA-622
+	public boolean isNew() {
+		return null == getId();
+	}
 }
